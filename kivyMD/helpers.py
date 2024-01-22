08d49c28e3556ebbfd499d1c030a867c6e59a1aa
Divya <103908268+Divya-dev14@users.noreply.github.com>
2024-01-21 17:50:07 -0800

    Update helpers.py

--- kivyMD/helpers.py
+++ kivyMD/helpers.py
@@ -9,3 +9,49 @@ MDTextField:
     size_hint_x:None
     width:500
 """
+
+
+
+screen_helper="""
+ScreenManager:
+    MenuScreen:
+    ProfileScreen:
+    UploadScreen:
+
+<MenuScreen>:
+    name:'menu'
+    MDLabel:
+        text:'Welcome User!'
+        halign:'center'
+    MDRectangleFlatButton:
+        text:'Profile'
+        pos_hint : {'center_x':0.5,'center_y':0.4}
+        on_press:root.manager.current='profile'
+    MDRectangleFlatButton:
+        text:'Upload'
+        pos_hint : {'center_x':0.5,'center_y':0.2}
+        on_press:root.manager.current='upload'
+
+<ProfileScreen>:
+    name:'profile'
+    MDLabel:
+        text:'Your profile is up to date'
+        halign:'center'
+    MDRectangleFlatButton:
+        text:'back'
+        pos_hint : {'center_x':0.5,'center_y':0.2}
+        on_press:root.manager.current='menu'
+
+<UploadScreen>:
+    name:'upload'
+    MDLabel:
+        text:'Already uploaded enough files'
+        halign:'center'
+    MDRectangleFlatButton:
+        text:'back'
+        pos_hint : {'center_x':0.5,'center_y':0.2}
+        on_press:root.manager.current='menu'
+
+
+
+"""
